e43f0386b43921048fb53f6490d0801fdd94dec0
Mayur Bhil <mayurbhil47@gmail.com>
2025-10-03 17:27:00 +0530

    add ws logic for room creation

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,33 @@
 import  express from "express";
+import jwt from "jsonwebtoken"
+import {JWT_SECRET} from "./config"
+import { middlware } from "./middlware";
+
 const app = express();
 const port  = 3001;
 
+
+app.post("/signup",()=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+    const userId = 1;
+
+    const  token = jwt.sign({   
+        userId
+    },JWT_SECRET)
+
+    res.json({
+
+    })
+})
+
+app.post("/room",middlware,(req,res)=>{
+    res.json({
+        roomId:132
+    })
+})
 
 app.listen(port,()=>{
     console.log(`server si listening on Portj ${port}`);
