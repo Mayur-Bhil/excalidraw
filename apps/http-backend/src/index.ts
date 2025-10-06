cceec463ce72793e0e4208b402befdc72ca9497c
Mayur Bhil <mayurbhil47@gmail.com>
2025-10-06 15:18:59 +0530

    created Firtpackage name :backend-common

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,35 +1,35 @@
-import  express from "express";
-import jwt from "jsonwebtoken"
-import {JWT_SECRET} from "./config"
-import { middlware } from "./middlware";
+    import  express from "express";
+    import jwt from "jsonwebtoken"
+    import {JWT_SECRET} from "@repo/backend-common/config"
+    import { middlware } from "./middlware";
 
-const app = express();
-const port  = 3001;
+    const app = express();
+    const port  = 3001;
 
 
-app.post("/signup",()=>{
-
-})
-
-app.post("/signin",(req,res)=>{
-    const userId = 1;
-
-    const  token = jwt.sign({   
-        userId
-    },JWT_SECRET)
-
-    res.json({
+    app.post("/signup",()=>{
 
     })
-})
 
-app.post("/room",middlware,(req,res)=>{
-    res.json({
-        roomId:132
+    app.post("/signin",(req,res)=>{
+        const userId = 1;
+
+        const  token = jwt.sign({   
+            userId
+        },JWT_SECRET)
+
+        res.json({
+
+        })
     })
-})
 
-app.listen(port,()=>{
-    console.log(`server si listening on Portj ${port}`);
-  
-})
+    app.post("/room",middlware,(req,res)=>{
+        res.json({
+            roomId:132
+        })
+    })
+
+    app.listen(port,()=>{
+        console.log(`server si listening on Portj ${port}`);
+    
+    })
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,6 @@
 import { WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from "jsonwebtoken"
-import { JWT_SECRET } from './config';
-
+import { JWT_SECRET } from "@repo/backend-common/config"
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function connection(ws,request) {
@@ -12,7 +11,7 @@ wss.on('connection', function connection(ws,request) {
 
   const queryParams = new URLSearchParams(url.split("?")[1]);
   const token = queryParams.get("token") || ""
-  const decoded  = jwt.verify(token,JWT_SECRET)
+  const decoded  = jwt.verify(token,JWT_SECRET) 
 
 
   if(!decoded || !(decoded as JwtPayload).userId){
