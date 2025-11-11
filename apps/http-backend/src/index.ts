90817ee23d506b054c601fd8d5d0e16f52d42386
Mayur Bhil <mayurbhil47@gmail.com>
2025-11-11 22:50:07 +0530

    writing frontends

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -225,6 +225,20 @@ app.get("/chats/:roomId",async(req,res)=>{
 
     res.json(messages)
 })
+
+
+
+app.get("/room/:slug",async(req,res)=>{
+    const slug  = req.params.slug;
+    
+   const room  = await prismaClient.room.findFirst({
+      where:{
+        slug 
+      }
+    })
+
+    res.json(room)
+})
     app.listen(port,()=>{
         console.log(`server is listening on http://localhost:${port} `);
     
