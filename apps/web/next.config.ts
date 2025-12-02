a9722bd56b8195a793162c7564e34b13e2a9fece
Mayur Bhil <mayurbhil47@gmail.com>
2025-12-02 17:20:47 +0530

    fix imports of the fiels

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages:["@repo/ui"],
+    turbopack: {
+    root: 'D:\\projects\\draw-app', // Absolute path
+  },
 };
 
 export default nextConfig;
---
+++ apps/web/src/types/css.d.ts
@@ -0,0 +1,9 @@
+declare module '@repo/ui/styles/globals.css' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: string;
+  export default content;
+}
