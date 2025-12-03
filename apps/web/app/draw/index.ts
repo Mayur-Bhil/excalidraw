f0b226370199538d1fb59df116cea938ebade0da
Mayur Bhil <mayurbhil47@gmail.com>
2025-12-03 17:13:16 +0530

    add first rectangle

---
+++ apps/web/app/draw/index.ts
@@ -0,0 +1,35 @@
+export default function initDraw(canvas:HTMLCanvasElement){
+            const ctx = canvas.getContext("2d");
+            
+            if(!ctx){
+                return;
+            }
+
+            ctx.strokeStyle = "rgba(255,255,255)"
+            ctx.fillRect(0,0,canvas.width,canvas.height)
+            let clicked = false;    
+            let startX = 0;
+            let startY = 0;
+            
+            canvas.addEventListener("mousedown",(e)=>{
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+
+             canvas.addEventListener("mouseup",(e)=>{
+                clicked = false
+                console.log("x : ",e.clientX);
+                console.log("y : ",e.clientY);
+            })
+
+             canvas.addEventListener("mousemove",(e)=>{
+                if(clicked){
+                    const width = e.clientX - startX;
+                    const height = e.clientY - startY;
+                    ctx.clearRect(0,0,canvas.width,canvas.height);
+                    ctx.fillStyle = "rgba(0,0,0)"   
+                    ctx.strokeRect(startX,startY,width,height)
+                }
+            })
+}
